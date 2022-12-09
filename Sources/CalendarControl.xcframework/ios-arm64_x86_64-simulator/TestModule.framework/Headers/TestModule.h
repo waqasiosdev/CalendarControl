bb9f925961b7b2ed0911ccd5adec90cc647aa337
Waqas Sarwar <waqas.sarwar@coeus-solutions.de>
2022-12-09 13:18:17 +0500

    initial commit

---
+++ Sources/CalendarControl.xcframework/ios-arm64_x86_64-simulator/TestModule.framework/Headers/TestModule.h
@@ -0,0 +1,18 @@
+//
+//  TestModule.h
+//  TestModule
+//
+//  Created by Coeus on 16/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestModule.
+FOUNDATION_EXPORT double TestModuleVersionNumber;
+
+//! Project version string for TestModule.
+FOUNDATION_EXPORT const unsigned char TestModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestModule/PublicHeader.h>
+
+
